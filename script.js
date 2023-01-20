5afc9bdd62437acb287ff76dd7e5b33bcd16f225
Pedro Navarro <54799795+pedronavarrodev@users.noreply.github.com>
2023-01-20 20:33:20 -0300

    att

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.raspadinha').addEventListener('click', function() {
+    document.querySelector('.codigo-de-rp').style.display = 'none';
+    document.querySelector('.codigo-de-rp2').style.display = 'none';
+    document.querySelector('.raspadinha').style.display = 'none';
+
+  });
